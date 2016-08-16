c7455c1705e50ecab8e963e5e79b24e25c6b08a5
Anna Zovaro <azovaro@Annas-dator.local>
2016-08-16 15:24:28 +1000

    First commit

---
+++ __init__.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm 
+from matplotlib import rc
+rc('image', interpolation='none', cmap = 'binary_r')
+
+# Plotting
+FIGSIZE = 5
+COLORBAR_FRACTION = 0.046
+COLORBAR_PAD = 0.04
+
+from plotutils import *
+
