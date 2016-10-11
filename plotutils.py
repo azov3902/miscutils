d002d673cd1b9da61a3d538f4e8bc02f8e9513de
Anna Zovaro <azovaro@Annas-dator.local>
2016-10-11 14:03:40 +1100

    Added arcsec tick marks to astroimshow()

--- plotutils.py
+++ plotutils.py
@@ -1,8 +1,9 @@
 from __future__ import division
 from miscutils import *
+from matplotlib.ticker import FormatStrFormatter
 
 ############################################################################################
-def colourbar(plot=None):
+def colorbar(plot=None):
 	""" Makes a nicely-sized colorbar. """
 	if plot:
 		plt.colorbar(plot,fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
@@ -18,7 +19,8 @@ def newfigure(height=1,width=1):
 def astroimshow(im, 
 	title=None,
 	plate_scale_as_px=1, 
-	subplot=None):
+	subplot=None,
+	colorbar_on=False):
 	""" Displays an input image given a plate scale. """
 	h, w = im.shape
 	if plt.is_numlike(subplot):
@@ -26,8 +28,12 @@ def astroimshow(im,
 	if title:
 		plt.title(title)
 	if plate_scale_as_px != 1:
-		plt.xlabel('arcsec')
-		plt.ylabel('arcsec')
+		# plt.xlabel('arcsec')
+		# plt.ylabel('arcsec')
+		plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%d\"'))
+		plt.gca().yaxis.set_major_formatter(FormatStrFormatter('%d\"'))
 	plt.imshow(im, extent = np.array([-w/2, w/2, -h/2, h/2]) * plate_scale_as_px)
+	if colorbar_on:
+		colorbar()
 	plt.show()
 
