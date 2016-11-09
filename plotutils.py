89506660b5273b18e589debe175fb8d7eb6a7757
Anna Zovaro <azovaro@Annas-dator.local>
2016-11-09 16:07:51 +1100

    Changed ### header to only occupy 80 characters

--- plotutils.py
+++ plotutils.py
@@ -2,7 +2,7 @@ from __future__ import division
 from miscutils import *
 from matplotlib.ticker import FormatStrFormatter
 
-############################################################################################
+################################################################################
 def colorbar(plot=None):
 	""" Makes a nicely-sized colorbar. """
 	if plot:
@@ -10,12 +10,12 @@ def colorbar(plot=None):
 	else:
 		plt.colorbar(fraction=COLORBAR_FRACTION, pad=COLORBAR_PAD)
 
-############################################################################################
+################################################################################
 def newfigure(height=1,width=1):
 	""" Makes a nicely-sized figure with width:height ratio = x:y. """
 	return plt.figure(figsize=(width*FIGSIZE,height*FIGSIZE))
 
-############################################################################################
+################################################################################
 def astroimshow(im, 
 	newwindow=False,
 	title=None,
@@ -39,7 +39,7 @@ def astroimshow(im,
 		colorbar()
 	plt.show()
 
-############################################################################################
+################################################################################
 def azElPlot(az_rad, el_rad,
 	title='',
 	linespec='ro',
