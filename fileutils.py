89c4fe9dcab3de86f2b9ac17419e1f6d249ba975
Anna Zovaro <azovaro@Annas-dator.local>
2017-01-04 09:53:36 +1100

    Changed function names to appropriate naming convention; added function to create a directory if it does not exist

--- fileutils.py
+++ fileutils.py
@@ -2,8 +2,9 @@ import os
 from os.path import isfile, join
 
 ################################################################################
-def countFilesInDirectory(directory):
-	# Counts the number of files (not including directories) in the given directory.
+def count_files_in_dir(directory):
+	# Counts the number of files (not including directories) in the given 
+	# directory.
 	return sum(1 for item in os.listdir(directory) if isfile(join(directory, item)))
 
 ################################################################################
@@ -12,6 +13,14 @@ def file_exists(file_path):
 	return isfile(file_path)
 
 ################################################################################
-def getFoldersInDirectory(directory = '.'):
-	# Returns a list containing names of all the immediate subdirectories in the given directory (the current directory by default)
+def get_folders_in_dir(directory = '.'):
+	# Returns a list containing names of all the immediate subdirectories in the 
+	# given directory (the current directory by default)
 	return next(os.walk(directory))[1]
+
+################################################################################
+def create_dir(directory):
+	# Creates the directory if it does not already exist.
+	if not os.path.exists(directory):
+		os.makedirs(directory)
+	return
